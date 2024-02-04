0dd60b66ed3ee8850372b5dc6503ab4036538fd2
BeluKotu <vietthanh456@gmail.com>
2024-02-04 15:30:43 +0700

    randomCommit

---
+++ src/components/book/bookSlice.js
@@ -0,0 +1,6 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../apiService.js";
+import { toast } from "react-toastify";
+
+export const bookSlice = createSlice({});
+export default bookSlice.reducer;
